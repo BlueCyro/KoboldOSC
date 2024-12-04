b10185392dbb417de9f67c16cf66f9d23efe3e94
bluecyro@protonmail.com <bluecyro@protonmail.com>
2024-12-04 02:22:54 -0600

    chore(cleanup)#: Comment out unused methods

--- KoboldOSC.cs
+++ KoboldOSC.cs
@@ -26,48 +26,48 @@ public static class KoboldOSC
     }
 
 
-    public static int GetOscPacketLength(ref OSCString path, params Span<IOSCParameter> parameters)
-    {
-        int pathBytes = path.AlignedLength;
-        int idTableBytes = (parameters.Length + 1).Ensure4Byte();
-        int count = pathBytes + idTableBytes;
+    // public static int GetOscPacketLength(ref OSCString path, params Span<IOSCParameter> parameters)
+    // {
+    //     int pathBytes = path.AlignedLength;
+    //     int idTableBytes = (parameters.Length + 1).Ensure4Byte();
+    //     int count = pathBytes + idTableBytes;
 
-        int paramBytes = 0;
-        for (int i = 0; i < parameters.Length; i++)
-            paramBytes += parameters[i].Length;
+    //     int paramBytes = 0;
+    //     for (int i = 0; i < parameters.Length; i++)
+    //         paramBytes += parameters[i].Length;
 
-        return count + paramBytes.Ensure4Byte();
-    }
-
-
-    public static void SerializeOscPacket(ref OSCString path, Span<byte> destination, params Span<IOSCParameter> parameters)
-    {
-        int pathBytes = path.AlignedLength;
-        int idTableBytes = (parameters.Length + 1).Ensure4Byte();
-
-        // Slice the first part of the OSC packet and copy the path to it
-        path.CopyTo(destination[..pathBytes]);
-
-        int realIndex = pathBytes;
-
-        Span<byte> idSlice = destination.Slice(realIndex, idTableBytes);
-        realIndex += idTableBytes;
-
-        idSlice[0] = MARKER;
-
-        for (int i = 0; i < parameters.Length; i++)
-            idSlice[i + 1] = parameters[i].ID;
+    //     return count + paramBytes.Ensure4Byte();
+    // }
 
 
-        Span<byte> paramSlice = destination[realIndex..];
+    // public static void SerializeOscPacket(ref OSCString path, Span<byte> destination, params Span<IOSCParameter> parameters)
+    // {
+    //     int pathBytes = path.AlignedLength;
+    //     int idTableBytes = (parameters.Length + 1).Ensure4Byte();
 
-        int curIndex = 0;
-        for (int i = 0; i < parameters.Length; i++)
-        {
-            var curParam = parameters[i];
-            curParam.CopyTo(paramSlice[curIndex..]);
+    //     // Slice the first part of the OSC packet and copy the path to it
+    //     path.CopyTo(destination[..pathBytes]);
 
-            curIndex += curParam.AlignedLength;
-        }
-    }
+    //     int realIndex = pathBytes;
+
+    //     Span<byte> idSlice = destination.Slice(realIndex, idTableBytes);
+    //     realIndex += idTableBytes;
+
+    //     idSlice[0] = MARKER;
+
+    //     for (int i = 0; i < parameters.Length; i++)
+    //         idSlice[i + 1] = parameters[i].ID;
+
+
+    //     Span<byte> paramSlice = destination[realIndex..];
+
+    //     int curIndex = 0;
+    //     for (int i = 0; i < parameters.Length; i++)
+    //     {
+    //         var curParam = parameters[i];
+    //         curParam.CopyTo(paramSlice[curIndex..]);
+
+    //         curIndex += curParam.AlignedLength;
+    //     }
+    // }
 }
